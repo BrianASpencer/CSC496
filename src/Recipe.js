4d8167c292de4335e97e476502b8b2a6d8274240
Brian Spencer <46761632+BrianASpencer@users.noreply.github.com>
2019-11-11 14:39:21 -0500

    Update Recipe.js

--- src/Recipe.js
+++ src/Recipe.js
@@ -5,8 +5,8 @@ const Recipe = ({title, imageURL, ingredients}) => {
     //console.log("imageURL"+imageURL);
     //console.log(imageURL);
     return (
-        <div data-testid="recipe" label = "lag">
-            <h1 data-testid="title">{title}</h1>
+        <div>
+            <h1>{title}</h1>
             <p>{ingredients}</p>
             <img src={"http://gtest.dev.wwbtc.com/"+imageURL} alt=""/>
         </div>
